2c4bfbb1a62dd206a5bb096e9c19b653a0c018e6
mshivam019 <mshivam019@gmail.com>
2023-06-20 15:40:48 +0530

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,23 @@
 import Contact from '@/components/Contact';
 import Container from '@/components/Container';
 import Experience from '@/components/Experience';
-
 import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 
+export async function getStaticProps() {
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
   useEffect(() => {
     Aos.init({ duration: 2000, once: true });
   }, []);
+
   return (
     <div className="dark:bg-neutral-800 ">
       <Container>
