6d0ba3748dd9f0c87ea49b02cd1e83251cf892fa
mshivam019 <mshivam019@gmail.com>
2023-06-20 16:13:19 +0530

    ISG

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const SECRET = process.env.MY_SECRET_TOKEN;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  try {
+    const { secret } = req.query;
+
+    // Check if the provided secret matches the expected secret
+    if (secret !== SECRET) {
+      res.status(401).json({ message: 'Invalid token' });
+      return;
+    }
+
+    // Trigger the revalidation for the index page
+    await res.revalidate('/');
+
+    res.status(200).json({ message: 'Revalidation triggered successfully' });
+  } catch (error) {
+    console.error('Error triggering revalidation:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
