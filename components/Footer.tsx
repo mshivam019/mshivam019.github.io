756679de975a553825a391f868bd2a5b5b25121a
Shivam Mishra <mshivam019@gmail.com>
2024-01-01 10:46:40 +0530

    make footer dynamic

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 
 export default function Footer() {
+  const date = new Date();
   return (
     <footer className=" sticky top-[100vh]">
       <div className="container mx-auto px-6 py-3 flex justify-between items-center">
         <div className="text-xl font-bold cursor-pointer">
           <Link href="/">Portfolio</Link>
         </div>
-        <p className="py-2  sm:py-0">All rights reserved © 2023</p>
+        <p className="py-2  sm:py-0">All rights reserved © {date.getFullYear()}</p>
       </div>
     </footer>
   );
