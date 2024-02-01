3eab3756b8cc60401cbda61f9b3d708350ef4266
Shivam Mishra <mshivam019@gmail.com>
2024-02-01 20:34:55 +0530

    Add motion animation to Container component

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Header from './Header';
 import Footer from './Footer';
+import { motion } from 'framer-motion';
 
 export default function Container(props: { color?: string; children: any }) {
   const router = useRouter();
@@ -13,38 +14,53 @@ export default function Container(props: { color?: string; children: any }) {
       'https://raw.githubusercontent.com/mshivam019/mshivam019/master/Banner.png',
     type: 'website',
   };
+  const variants = {
+    hidden: { opacity: 0, x: -200, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: -100 },
+  };
   return (
-    <div className={color || ''}>
-      <Head>
-        <link rel="icon" type="image/png" href="/image.png" />
+    <motion.main
+      initial="hidden"
+      animate="enter"
+      exit="exit"
+      variants={variants}
+      transition={{ type: 'linear' }}
+      className=""
+    >
+      <div className={color || ''}>
+        <Head>
+          <link rel="icon" type="image/png" href="/image.png" />
 
-        <title>{meta.title}</title>
-        <meta name="robots" content="follow, index" />
-        <meta content={meta.description} name="description" />
-        <meta
-          property="og:url"
-          content={`https://mshivam019.vercel.app/${router.asPath}`}
-        />
-        <link
-          rel="canonical"
-          href={`https://mshivam019.vercel.app/${router.asPath}`}
-        />
-        <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Shivam Mishra" />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mshivam0019" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
-      </Head>
-      <main className="dark:bg-neutral-800">
-        <Header color={color} />
-        <div>{children}</div>
-        <Footer color={color} />
-      </main>
-    </div>
+          <title>{meta.title}</title>
+          <meta name="robots" content="follow, index" />
+          <meta content={meta.description} name="description" />
+          <meta
+            property="og:url"
+            content={`https://mshivam019.vercel.app/${router.asPath}`}
+          />
+          <link
+            rel="canonical"
+            href={`https://mshivam019.vercel.app/${router.asPath}`}
+          />
+          <meta property="og:type" content={meta.type} />
+          <meta property="og:site_name" content="Shivam Mishra" />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:image" content={meta.image} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@mshivam0019" />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta name="twitter:image" content={meta.image} />
+        </Head>
+        <main className="dark:bg-neutral-800">
+          <Header color={color} />
+
+          <div>{children}</div>
+          <Footer color={color} />
+        </main>
+      </div>
+    </motion.main>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { AppProps } from 'next/app';
+import { AnimatePresence } from 'framer-motion';
 
 export default function App({
   Component,
@@ -14,7 +15,13 @@ export default function App({
   return (
     <ThemeProvider attribute="class">
       <main>
-        <Component {...pageProps} />
+        <AnimatePresence
+          mode="wait"
+          initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <Component {...pageProps} />
+        </AnimatePresence>
         <SpeedInsights />
         <Analytics />
       </main>
