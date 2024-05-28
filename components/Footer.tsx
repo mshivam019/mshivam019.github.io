ee96679996238a0766d9e6792c845707454e30da
Shivam Mishra <mshivam019@gmail.com>
2024-05-28 11:21:28 +0530

    update sitemap

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Footer() {
   const date = new Date();
   return (
-    <footer className=" sticky top-[100vh]">
+    <footer className="sticky top-[100vh]">
       <div className="container mx-auto px-6 py-3 flex justify-between items-center">
         <div className="text-xl font-bold cursor-pointer">
           <Link href="/">Portfolio</Link>
